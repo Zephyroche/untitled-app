dd34393b9018c8edd9758bf6e8cfad24d4659fc0
Zephyroche <pvarpella11@gmail.com>
2022-12-16 20:07:22 +0300

    Update styles and logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import TicketCard from './TicketCard';
 import logo from './Logo.svg'
 import './App.css';
 
 const App = () => {
+    const [tickets, setTickets] = useState([]);
+
+    const ticketList = async() => {
+        const response = await fetch(`./tickets/Tickets.json`);
+        const data = await response.json();
+
+        setTickets(data.tickets);
+    }
+
+    useEffect(() => {
+        ticketList()
+    }, []);
+
+    
+
     return (
         <div className='body'>
             <div className='logo-row'>
                 <img src={logo} className='logo'/>
             </div>
-            <div className='filter'>
-                
-            </div>
-            <div className='tabs'>
-                
-            </div>
-            <div className='ticket'>
-
+            <div className='filter'></div>
+            <div className='tabs'></div>
+            <div>
+                {tickets.map((ticket, index) => (
+                    <TicketCard ticket={ticket} key={index}/>
+                ))}
             </div>
         </div>
     );
