bf17d1799aaca3adf8a93acd2d44fca5a4fd971c
Zephyroche <pvarpella11@gmail.com>
2022-12-16 11:20:56 +0300

    Add some files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './Logo.svg'
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='body'>
+            <div className='logo-row'>
+                <img src={logo} className='logo'/>
+            </div>
+            <div className='filter'>
+                
+            </div>
+            <div className='tabs'>
+                
+            </div>
+            <div className='ticket'>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
